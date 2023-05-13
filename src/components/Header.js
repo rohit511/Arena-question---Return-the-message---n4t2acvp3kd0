dc5feaf856eccbeff73938f3a2507c90c170e1dc
Rohit Singh Rathore <96055471+rohit511@users.noreply.github.com>
2023-05-13 09:45:50 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
 
     return (
-       <div id="header-component">Newton School</div>
+       <div id="header-component"><h1>Newton School</h1></div>
     )
 }
 
